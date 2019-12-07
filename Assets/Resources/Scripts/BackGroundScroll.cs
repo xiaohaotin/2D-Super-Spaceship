c5a441beb22434e696564cee3bd41f05ded345c6
xht <xht19970808@gmail.com>
2019-12-07 23:48:23 +0900

    新しいEnemyPrefabとBulletPrefabを作成しました。　

---
+++ Assets/Resources/Scripts/BackGroundScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroll : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+    public int xVelocity, yVelocity;
+
+    void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}

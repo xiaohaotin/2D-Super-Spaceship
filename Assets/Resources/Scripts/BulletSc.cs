6c6e658579ccc7abf91f334244f4770169418418
xht <>
2019-12-17 12:08:06 +0900

    bullet luncher script 追加修正

---
+++ Assets/Resources/Scripts/BulletSc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSc : MonoBehaviour
+{
+    public float Velocity_0, theta;
+
+    Rigidbody2D rid2d;
+    void Start() {
+        //Rigidbody取得
+        rid2d = GetComponent<Rigidbody2D>();
+        //角度を考慮して弾の速度計算
+        Vector2 bulletV = rid2d.velocity;
+        bulletV.x = Velocity_0 * Mathf.Cos(theta);
+        bulletV.y = Velocity_0 * Mathf.Sin(theta);
+        rid2d.velocity = bulletV;
+    }
+}
---
+++ Assets/Resources/Scripts/CircleLuncher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleLuncher : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float _Velocity_0, Degree, Angle_Split;
+
+    float _theta;
+    float PI = Mathf.PI;
+
+
+    // Update is called once per frame
+    void Update() {
+        for (int i = 0; i <= (Angle_Split - 1); i++) {
+            //n-way弾の端から端までの角度
+            float AngleRange = PI * (Degree / 180);
+
+            //弾インスタンス渡す角度の計算
+            if (Angle_Split > 1) {
+                _theta = (AngleRange / (Angle_Split - 1)) * i + 0.5f * (PI - AngleRange);
+            }
+            else 
+            {
+                _theta = 0.5f * PI;
+            }
+
+            //弾インスタンスを取得し、初速度と発射角度を与える
+            GameObject Bullet_obj = (GameObject)Instantiate(Bullet, transform.position, transform.rotation);
+            BulletSc bullet = Bullet_obj.GetComponent<BulletSc>();
+            //bullet_cs.theta = _theta;
+            //bullet_cs.Velocity_0 = _Velocity_0;
+        }
+    }
+}
